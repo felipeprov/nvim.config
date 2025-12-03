593a1974466a2562875f4c12ba3edf767b371b24
Felipe PROVENZANO <fprovenzano@solent.fr>
2025-12-03 13:37:44 +0100

    added diagnostics

--- init.lua
+++ init.lua
@@ -31,6 +31,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 require('config.lazy')
+
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show error" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Send to loclist" })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
 --vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
 ---- Next buffer
 --vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Next buffer" })
